5939f98f5aa507cc6bf58761d944fbd774189ae2
Akshay <akshaymangutkar951@gmail.com>
2025-03-31 12:45:59 +0530

    modified the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 const Navbar = () => {
   return (
-    <div>
-      <nav className="bg-gray-900 text-white py-4 px-6 flex justify-center items-center shadow-md">
-        <h1 className="text-3xl font-extrabold tracking-wide text-gray-100 uppercase relative ">
+    <div className="w-full bg-gray-900 text-white shadow-md">
+      <nav className="container mx-auto py-4 px-6 flex flex-wrap justify-between items-center">
+        <h1 className="text-2xl md:text-3xl font-extrabold tracking-wide uppercase">
           A.V. Engineering
-          <span className="text-sm text-gray-400 italic  top-3 ml-4">
+          <span className="block text-sm text-gray-400 italic md:inline ml-2">
             Precision is our key
           </span>
         </h1>
